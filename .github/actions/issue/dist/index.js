3283db33d5fa98dcae0a6ca2bcdf0228babe1883
Christophe Pruvost <christophe.pruvost@oracle.com>
2022-06-21 17:07:12 +0200

    feat: test

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -4917,29 +4917,35 @@ Object.defineProperty(exports, "toPlatformPath", { enumerable: true, get: functi
 
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
+const { Octokit } = __webpack_require__(571);
 
-try {
-    const token = core.getInput('token');
-    const title = core.getInput('title');
-    const body = core.getInput('body');
-    const assignees = core.getInput('assignees');
+async function run() {
+    try {
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
 
-    const oktopic = new github.Github(token);
+    //const oktokit = new github.Github(token);
+    const octokit = new Octokit();
 
-    const response = octokit.rest.issues.create({
+    const response = await octokit.rest.issues.create({
         //owner: github.context.repo.owner,
         //repo: github.context.repo.repo,
         ...github.context.repo,
         title,
         body,
-        assignees: assignees ? assignees.split('\n') : undefined
+        assignees: assignees ? assignees.split("\n") : undefined,
     });
 
-    core.setOutput('issue', JSON.stringify(response.data));
-
-} catch (error) {
+    core.setOutput("issue", JSON.stringify(response.data));
+    } catch (error) {
     core / core.setFailed(error.message);
-}    
+    }
+}
+
+run();
+
 
 /***/ }),
 
@@ -5206,6 +5212,14 @@ class PersonalAccessTokenCredentialHandler {
 }
 exports.PersonalAccessTokenCredentialHandler = PersonalAccessTokenCredentialHandler;
 //# sourceMappingURL=auth.js.map
+
+/***/ }),
+
+/***/ 571:
+/***/ (function(module) {
+
+module.exports = eval("require")("@octokit/action");
+
 
 /***/ }),
 
--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -1,25 +1,30 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const { Octokit } = require("@octokit/action");
 
-try {
-    const token = core.getInput('token');
-    const title = core.getInput('title');
-    const body = core.getInput('body');
-    const assignees = core.getInput('assignees');
+async function run() {
+    try {
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
 
-    const oktopic = new github.Github(token);
+    //const oktokit = new github.Github(token);
+    const octokit = new Octokit();
 
-    const response = octokit.rest.issues.create({
+    const response = await octokit.rest.issues.create({
         //owner: github.context.repo.owner,
         //repo: github.context.repo.repo,
         ...github.context.repo,
         title,
         body,
-        assignees: assignees ? assignees.split('\n') : undefined
+        assignees: assignees ? assignees.split("\n") : undefined,
     });
 
-    core.setOutput('issue', JSON.stringify(response.data));
-
-} catch (error) {
+    core.setOutput("issue", JSON.stringify(response.data));
+    } catch (error) {
     core / core.setFailed(error.message);
-}    
+    }
+}
+
+run();
